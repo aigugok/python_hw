28ffba59f5b1c0aea9bec0e32a775ca04b5dd388
Aigul <user@localhost>
2021-08-18 21:45:53 +0300

    hw16

---
+++ hw16.py
@@ -0,0 +1,23 @@
+#С помощью бибилиотеки geopy можно узнать местоположение по ширине и долготе (сервис Nominatim),url для Goggle Maps сформирован "в лоб" c помощью f-string
+from geopy.geocoders import Nominatim
+geolocator = Nominatim(user_agent="myapp")
+
+
+def gps_data_location_and_url(gps_data):
+    try:
+        geolocator.reverse(gps_data)
+    except ValueError:
+        return f"Not allowed format of GPS data"
+    location = geolocator.reverse(gps_data)
+    return f"Location: {location.address}\nGoggle Maps URL: https://www.google.com/maps/search/?api=1&query={gps_data}"
+
+
+
+
+def gps_data_from_file(filename):
+    with open(filename,"r") as f:
+        for line in f:
+            line=line.replace("'","").replace(";",",")
+            print(gps_data_location_and_url(line))
+
+gps_data_from_file("gps_data.txt")
