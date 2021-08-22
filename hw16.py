546c75fa39496b4020c07153393cf211d3f71ad6
Aigul <user@localhost>
2021-08-22 23:55:25 +0300

    hw16

--- hw16.py
+++ hw16.py
@@ -1,5 +1,6 @@
 #С помощью бибилиотеки geopy можно узнать местоположение по ширине и долготе (сервис Nominatim),url для Goggle Maps сформирован "в лоб" c помощью f-string
 from geopy.geocoders import Nominatim
+import requests
 geolocator = Nominatim(user_agent="myapp")
 
 
@@ -13,13 +14,18 @@ def gps_data_location_and_url(gps_data):
 
 
 
-
 def gps_data_from_file(filename):
     with open(filename,"r") as f:
         for line in f:
-            line=line.replace("'","").replace(";",",")
-            print(gps_data_location_and_url(line))
+            line=line.replace(",",".").split(";")
+            for i in range(len(line)):
+                degres=line[i].split(".")[0]
+                line[i]=line[i].replace(degres+".","")
+                minutes=line[i].split("'")[0]
+                line[i]= str((int(minutes)/60) + int(degres))
+            print(gps_data_location_and_url(",".join(line)))
 
 
 #запуск программы
 # gps_data_from_file("gps_data.txt")
+
