8725a05e1d63e64988efbcfe624fd8b1cad37773
Aigul <user@localhost>
2021-08-22 15:07:57 +0300

    hw10

--- hw10.py
+++ hw10.py
@@ -1,19 +1,19 @@
-#Пол-ль вводит натуральное число, на выходе кол-во шагов для ф-ии Коллатца необходимых для достижения этим числом единицы
+# Пол-ль вводит натуральное число, на выходе кол-во шагов для ф-ии Коллатца необходимых для достижения этим числом единицы
 
-def Collatz_hypothesis(n,step=0):
-    if (n<1):
-        print("это не натуральное число")
+def Collatz_hypothesis(n, step=0):
+    if (n < 1):
+        return "это не натуральное число"
         return
-    if n==1:
-        print(f"Всего получаем {step} шагов.")
+    if n == 1:
+        return f"Всего получаем {step} шагов."
         return
-    if(n%2==0):
-        return Collatz_hypothesis(n//2,step+1)
-    return Collatz_hypothesis(n*3+1,step+1)
+    if (n % 2 == 0):
+        return Collatz_hypothesis(n // 2, step + 1)
+    return Collatz_hypothesis(n * 3 + 1, step + 1)
 
 
 try:
-    num=int(input())
-    Collatz_hypothesis(num)
+    num = int(input())
+    print(Collatz_hypothesis(num))
 except ValueError:
     print("Не удалось преобразовать введенный текст в число.")
